6b01a3ce2ee7688e9bab610e21ff1cbe9cf5054b
Rolando Carrasco <rcarrasco@spsolutions.com.mx>
2024-03-29 11:52:09 -0600

    test

--- mule-tester/background.sh
+++ mule-tester/background.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 # Update and Upgrade packages
+echo "Updating the system"
 apt update -y
+echo "Upgrading the system"
 apt upgrade -y
 apt install curl -y
+
+echo "Downloading nvm..."
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 source ~/.bashrc
+
+echo "Installing Node..."
 nvm install --lts
 nvm alias default 20.12.0
 
@@ -14,6 +20,6 @@ nvm alias default 20.12.0
 node -v npm -v
 
 touch /root/allSetInBackground
-
+echo "All set, you can proceed to the next step."
 
 
